174a04d2bb3bf214fd9a2edb29d4ad976f7feef7
Bruno Konrad <bruno.skon@gmail.com>
2015-03-25 12:23:23 -0300

    Loop de animação
    Implementação de um loop de animação seguindo o segundo capítulo

---
+++ src/animacao.js
@@ -0,0 +1,45 @@
+window.Animacao = (function() {
+  function Animacao(context) {
+    this.context = context;
+    this.sprites = [];
+    this.ligado = false;
+  }
+  Animacao.prototype = {
+    novoSprite: function(sprite) {
+      this.sprites.push(sprite);
+    },
+    ligar: function() {
+      this.ligado = true;
+      this.proximoFrame();
+    },
+    desligar: function() {
+      this.ligado = false; 
+    },
+    proximoFrame: function() {
+      if (!this.ligado)
+        return;
+      
+      this.limparTela();
+      
+      for (var i in this.sprites) {
+        this.sprites[i].atualizar();
+      }
+      
+      for (var i in this.sprites) {
+        this.sprites[i].desenhar(); 
+      }
+      
+      var animacao = this;
+      requestAnimationFrame(function() {
+        animacao.proximoFrame();
+      });
+    },
+    limparTela: function() {
+      this.context.clearRect(0, 0, this.context.canvas.width,
+                             this.context.canvas.height);
+      
+    }
+  };
+  
+  return Animacao;
+})();
